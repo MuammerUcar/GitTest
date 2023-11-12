ea4c4b58c40bf03441ea06155d4dc4a9db224df9
MuammerUcar <144108438+MuammerUcar@users.noreply.github.com>
2023-11-12 11:41:26 +0300

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,5 +2,6 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("Git den merhaba");
         System.out.println("GitHup dan merhaba");
+        System.out.println("GitHup dan tekrar merhaba");
     }
 }
